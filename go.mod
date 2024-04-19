5b8a481b3993895825fe80e88476d6a300841e65
Matteo Joliveau <matteo@matteojoliveau.com>
2024-04-19 18:36:39 +0200

    feat(server): implement user creation and password verification

--- go.mod
+++ go.mod
@@ -9,16 +9,20 @@ require (
 	github.com/stretchr/testify v1.8.3
 )
 
+require golang.org/x/crypto v0.17.0 // indirect
+
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.6.0
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/matthewhartstonge/argon2 v1.0.0
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.5.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f
 	golang.org/x/sys v0.15.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ server/internal/core/user/user_test.go
@@ -0,0 +1,47 @@
+package user
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUser(t *testing.T) {
+	t.Run("can be created with valid parameters", func(t *testing.T) {
+		username := "test"
+		password := "testpassword"
+
+		created, err := Create(username, password)
+
+		assert.NoError(t, err)
+		assert.NotEqual(t, UserId(uuid.Nil), created.Id)
+		assert.Equal(t, username, created.Username)
+	})
+
+	t.Run("verifies a valid password", func(t *testing.T) {
+		username := "test"
+		password := "testpassword"
+
+		user, err := Create(username, password)
+
+		assert.NoError(t, err)
+
+		valid, err := user.VerifyPassword(password)
+		assert.NoError(t, err)
+		assert.True(t, valid)
+	})
+
+	t.Run("does not verify an invalid password", func(t *testing.T) {
+		username := "test"
+		password := "testpassword"
+
+		user, err := Create(username, password)
+
+		assert.NoError(t, err)
+
+		valid, err := user.VerifyPassword("invalid password")
+		assert.NoError(t, err)
+		assert.False(t, valid)
+	})
+}
