341eb01cc5fabf4802aa58b1e384d744674afab9
Matteo Joliveau <matteo@matteojoliveau.com>
2024-05-07 16:59:51 +0200

    chore(server): add scheduled job system
    Signed-off-by: Matteo Joliveau <matteo@matteojoliveau.com>

--- go.mod
+++ go.mod
@@ -43,6 +43,7 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jonboulle/clockwork v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
@@ -62,6 +63,7 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/shirou/gopsutil/v3 v3.23.12 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
@@ -96,6 +98,7 @@ require (
 require (
 	github.com/alitto/pond v1.8.3
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-co-op/gocron/v2 v2.5.0
 	github.com/go-http-utils/headers v0.0.0-20181008091004-fed159eddc2a
 	github.com/google/uuid v1.6.0
 	github.com/hashicorp/errwrap v1.1.0 // indirect
--- server/internal/worker/pool_test.go
+++ server/internal/worker/pool_test.go
@@ -5,22 +5,35 @@ import (
 	"testing"
 	"time"
 
+	"github.com/oxidrive/oxidrive/server/internal/testutil"
+	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 type testJob struct {
+	done  chan struct{}
 	sleep time.Duration
 }
 
 var _ Job = (*testJob)(nil)
 
+func (j *testJob) Name() string {
+	return "test job"
+}
+
 func (j *testJob) Run(_ context.Context) error {
 	time.Sleep(j.sleep)
+	if j.done != nil {
+		j.done <- struct{}{}
+	}
 	return nil
 }
 
 func TestWorkerPool(t *testing.T) {
 	t.Run("runs a background job", func(t *testing.T) {
+		t.Parallel()
+
 		ctx, done := context.WithTimeout(context.Background(), 200*time.Millisecond)
 		defer done()
 
@@ -33,11 +46,13 @@ func TestWorkerPool(t *testing.T) {
 		case err := <-j.Wait():
 			assert.NoError(t, err)
 		case <-ctx.Done():
-			t.Fail()
+			t.Fatal("context timeout expired")
 		}
 	})
 
 	t.Run("runs a background job for too long", func(t *testing.T) {
+		t.Parallel()
+
 		ctx, done := context.WithTimeout(context.Background(), 200*time.Millisecond)
 		defer done()
 
@@ -48,9 +63,37 @@ func TestWorkerPool(t *testing.T) {
 
 		select {
 		case <-j.Wait():
-			t.FailNow()
+			t.Fatal("received result instead of timeout expiring")
 		case <-ctx.Done():
 			// Pass
+		}
+	})
+}
+
+func TestWorkerScheduler(t *testing.T) {
+	t.Run("schedules a background job", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, done := context.WithTimeout(context.Background(), 3*time.Second)
+		defer done()
+
+		testutil.IntegrationTest(ctx, t)
+
+		p, err := NewScheduler(zerolog.New(zerolog.NewTestWriter(t)))
+		require.NoError(t, err)
+		defer p.Shutdown()
+
+		ch := make(chan struct{})
+
+		_, err = p.ScheduleEvery(ctx, 1*time.Second, &testJob{done: ch})
+		require.NoError(t, err)
+
+		p.Start()
+
+		select {
+		case <-ch:
+		case <-ctx.Done():
+			t.Fatal("context timeout expired")
 		}
 	})
 }
