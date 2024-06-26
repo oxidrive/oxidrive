9b25f5493c7254cb09774a7a5fa421c90920bdb6
Matteo Joliveau <matteo@matteojoliveau.com>
2024-06-26 12:00:42 +0200

    chore: use lefthook instead of pre-commit for formatting checks
    Signed-off-by: Matteo Joliveau <matteo@matteojoliveau.com>

--- flake.nix
+++ flake.nix
@@ -4,17 +4,14 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
     devshell.url = "github:numtide/devshell";
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
-        inputs.pre-commit-hooks-nix.flakeModule
         inputs.devshell.flakeModule
 
-        ./nix/docs.nix
         ./nix/go.nix
         ./nix/node.nix
         ./nix/shell.nix
