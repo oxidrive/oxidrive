ab43962a129c4804d713db10a9d4ab11f17fde5c
Matteo Joliveau <matteo@matteojoliveau.com>
2024-07-02 17:45:13 +0200

    chore(e2e): update test suite to include file renaming and deletion
    Signed-off-by: Matteo Joliveau <matteo@matteojoliveau.com>

--- e2e/tests/file-operations.spec.ts
+++ e2e/tests/file-operations.spec.ts
@@ -6,7 +6,9 @@ test(
 	testAccessibility("/files"),
 );
 
-test("should upload, preview, and download a file", async ({ page }) => {
+test("should upload, preview, download, and delete a file", async ({
+	page,
+}) => {
 	const name = `hello${Math.random().toString().substring(2)}.json`;
 	const content = '{"hello":"world!"}';
 
@@ -17,9 +19,68 @@ test("should upload, preview, and download a file", async ({ page }) => {
 	const file = await test.step("Upload file", upload({ page, name, content }));
 	await test.step("Preview file", preview(file, { page, name, content }));
 	await test.step("Download file", download(file, { page, name, content }));
+	await test.step("Delete file", remove(file, { page, name, content }));
 });
 
-function upload({ page, name, content }: StepArgs): Step<Locator> {
+test("should upload, rename, and delete a file", async ({ page }) => {
+	const run = Math.random().toString().substring(2);
+	const name = `hello${run}.json`;
+	const newName = `changed${run}.json`;
+	const content = '{"hello":"world!"}';
+
+	await page.goto("/files");
+	await page.waitForLoadState("networkidle");
+	await expect(page).toHaveURL("/files");
+
+	const file = await test.step("Upload file", upload({ page, name, content }));
+	const renamed = await test.step(
+		"Rename file",
+		rename(file, newName, { page, name, content }),
+	);
+	await test.step(
+		"Delete file",
+		remove(renamed, { page, name: newName, content }),
+	);
+});
+
+test("should upload, and rename a folder", async ({ page }) => {
+	const run = Math.random().toString().substring(2);
+	const folderName = `tests${run}`;
+	const newFolderName = `changed${run}`;
+	const fileName = `hello${run}.json`;
+	const name = `${folderName}/${fileName}`;
+	const content = '{"hello":"world!"}';
+
+	await page.goto("/files");
+	await page.waitForLoadState("networkidle");
+	await expect(page).toHaveURL("/files");
+
+	await test.step(
+		"Upload file",
+		upload({ page, name, content, folder: folderName }),
+	);
+	const folder = page.getByTitle(folderName, { exact: true }).first();
+	await test.step(
+		"Rename folder",
+		rename(folder, newFolderName, { page, name, content }),
+	);
+
+	await page.getByRole("link", { name: newFolderName, exact: true }).click();
+
+	await expect(page).toHaveURL(`/files/${newFolderName}`);
+
+	const file = page.getByTitle(fileName, { exact: true }).first();
+	await test.step(
+		"Preview file",
+		preview(file, { page, name: fileName, content }),
+	);
+	await test.step(
+		"Delete file",
+		remove(file, { page, name: fileName, content }),
+	);
+});
+
+function upload({ page, name, content, folder }: StepArgs): Step<Locator> {
 	return async () => {
 		await expect(page.getByText(name)).not.toBeVisible();
 
@@ -50,13 +111,13 @@ function upload({ page, name, content }: StepArgs): Step<Locator> {
 
 		await expect(page.getByText("No files in here")).not.toBeVisible();
 
-		const file = page.getByTitle(name, { exact: true }).first();
+		const file = page.getByTitle(folder ?? name, { exact: true }).first();
 		await expect(file).toBeVisible();
 		return file;
 	};
 }
 
-function preview(file: Locator, { page, name, content }: StepArgs): Step {
+function preview(file: Locator, { page, name }: StepArgs): Step {
 	return async () => {
 		await file.click();
 
@@ -97,10 +158,54 @@ function download(file: Locator, { page, name }: StepArgs): Step {
 	};
 }
 
+function rename(
+	file: Locator,
+	newName: string,
+	{ page }: StepArgs,
+): Step<Locator> {
+	return async () => {
+		const actions = file.getByTestId("file-actions");
+
+		await expect(actions).toBeVisible();
+		await actions.click();
+
+		const action = page.getByRole("menuitem", { name: "Rename" });
+		await expect(action).toBeVisible();
+		await action.click();
+
+		const input = page.getByRole("textbox");
+		await expect(input).toBeVisible();
+		await input.fill(newName);
+		await page.keyboard.press("Enter");
+
+		const renamed = page.getByTitle(newName, { exact: true }).first();
+		await expect(renamed).toBeVisible();
+		return renamed;
+	};
+}
+
+function remove(file: Locator, { page }: StepArgs): Step {
+	return async () => {
+		page.on("dialog", (dialog) => dialog.accept());
+
+		const actions = file.getByTestId("file-actions");
+
+		await expect(actions).toBeVisible();
+		await actions.click();
+
+		const action = page.getByRole("menuitem", { name: "Delete" });
+		await expect(action).toBeVisible();
+		await action.click();
+
+		await expect(file).not.toBeVisible();
+	};
+}
+
 interface StepArgs {
 	page: Page;
 	name: string;
 	content: string;
+	folder?: string;
 }
 
 type Step<T = void> = () => Promise<T>;
