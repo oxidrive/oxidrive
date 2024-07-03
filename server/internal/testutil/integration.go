28823d18f0969cf717c895b97537dacec364affe
Matteo Joliveau <matteo@matteojoliveau.com>
2024-07-03 12:35:19 +0200

    chore(server): use testing.Short() instead of custom envar to disable integration tests
    Signed-off-by: Matteo Joliveau <matteo@matteojoliveau.com>

--- server/internal/testutil/integration.go
+++ server/internal/testutil/integration.go
@@ -2,28 +2,17 @@ package testutil
 
 import (
 	"context"
-	"os"
-	"strings"
 	"testing"
 )
 
-func isEnvVarTrue(value string) bool {
-	switch strings.ToLower(value) {
-	case "y", "yes", "t", "true", "1":
-		return true
-	default:
-		return false
-	}
-}
-
 type IntegrationDependency func(context.Context, *testing.T) (context.Context, func())
 
-// IntegrationTest skips a test if the INTEGRATION_TEST env variable is not set while running go tests
+// IntegrationTest skips a test if the -short flag is not set while running go tests
 func IntegrationTest(ctx context.Context, t *testing.T, integrationDeps ...IntegrationDependency) (context.Context, func()) {
 	t.Helper()
 
-	if value, ok := os.LookupEnv("INTEGRATION_TEST"); !ok || !isEnvVarTrue(value) {
-		t.Skip("skipping integration tests, call go test with INTEGRATION_TEST set")
+	if testing.Short() {
+		t.Skip("skipping integration tests, call go test without -short to run them")
 	}
 
 	closeFunctions := make([]func(), len(integrationDeps))
