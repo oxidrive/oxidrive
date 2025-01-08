06ca7cb57c005c86d5a71add4db4f8da116d150b
Matteo Joliveau <matteo@matteojoliveau.com>
2025-01-08 14:44:30 +0100

    chore(ci): fix web and server runs
    Signed-off-by: Matteo Joliveau <matteo@matteojoliveau.com>

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,16 +1,16 @@
+import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
-import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-    plugins: [sveltekit()],
+	plugins: [sveltekit()],
 
-    test: {
-        include: ['src/**/*.{test,spec}.{js,ts}'],
-    },
+	test: {
+		include: ["src/**/*.{test,spec}.{js,ts}"],
+	},
 
-    server: {
-        proxy: {
-            '/api': 'http://localhost:4000'
-        },
-    },
+	server: {
+		proxy: {
+			"/api": "http://localhost:4000",
+		},
+	},
 });
