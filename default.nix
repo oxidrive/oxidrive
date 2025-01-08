0d74081ee8c4dfac3c3aa4b778a82704acc24b16
Matteo Joliveau <matteo@matteojoliveau.com>
2025-01-08 17:16:49 +0100

    chore(server): change nix build
    Signed-off-by: Matteo Joliveau <matteo@matteojoliveau.com>

--- default.nix
+++ default.nix
@@ -1,18 +1,19 @@
 { pkgs
 , lib
 , rustPlatform
-, web
 }:
 
 let
-  manifest = lib.importTOML ../Cargo.toml;
+  manifest = lib.importTOML ./Cargo.toml;
   meta = manifest.workspace.package;
+
+  web = pkgs.callPackage ./nix/web.nix { };
 in
 rustPlatform.buildRustPackage {
   pname = "oxidrive";
   version = meta.version;
-  cargoLock.lockFile = ../Cargo.lock;
-  src = lib.cleanSource ../.;
+  cargoLock.lockFile = ./Cargo.lock;
+  src = lib.cleanSource ./.;
 
   useNextest = true;
 
